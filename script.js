733261c82da0dce200c26966846004f16dbc54f2
Pratham131 <104488785+Pratham131@users.noreply.github.com>
2023-07-20 17:12:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+const lineElement = document.getElementById('line');
+
+function rotateLine() {
+    let rotation = 0;
+    const rotationInterval = 5; // Adjust the rotation speed here (smaller value for faster rotation)
+    
+    function animateRotation() {
+        rotation += rotationInterval;
+        lineElement.style.transform = `rotate(${rotation}deg)`;
+        requestAnimationFrame(animateRotation);
+    }
+
+    animateRotation();
+}
+
+rotateLine();
